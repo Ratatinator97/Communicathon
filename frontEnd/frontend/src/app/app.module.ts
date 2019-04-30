ffa68f6196ca9ea0479361f2156871ea5b23c887
bdecarvalho <biavaz_96@hotmail.com>
2019-04-30 17:42:39 +0200

    mmmm

--- frontEnd/frontend/src/app/app.module.ts
+++ frontEnd/frontend/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TaskService } from './services/task.service';
+import { TodoService } from './services/todo.service';
+//heey
 @NgModule({
   declarations: [
     AppComponent
