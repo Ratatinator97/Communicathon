2fa03a50556a219df48be5a35a5e56a32a2cbd44
Alexandros SIDIRAS <alex@Arch2.localdomain>
2019-05-01 14:50:05 +0200

    Trying things

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,12 +1,21 @@
+
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+/*
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-var user = require('./routes/user');
-var cardID = require('./routes/cardID');
-const app = express();
-const router = express.Router();
 
+var user = require('./routes/user');
+
+var cardID = require('./routes/cardID');
+*/
+const port = 4000;
+const app = express();
+//const router = express.Router();
+/*
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -17,8 +26,11 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
 });
-
-app.use('/', router);
-app.use('/user', user);
-app.use('user/cardID',cardID);
-app.listen(4000, () => console.log(`Express server running on port 4000`))
+*/
+// DEPRECATED app.use('/', router);
+// DEPRECATED app.use('/user', user);
+// DEPRECATED app.use('user/cardID',cardID);
+require('./routes')(app, {});
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
