68112418f52046acbac07a036aae7cb714a31954
Alexandros SIDIRAS <alex@Arch2.localdomain>
2019-05-01 11:07:37 +0200

    MongoDB schemes updated

---
+++ backEnd/models/CartID.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var cardIDSchema = new mongoose.Schema({
+  userID: String,
+  name: String,
+  surname: Number,
+  date_Of_Birth: Date,
+  address: String,
+  personnal_Email: String,
+  personnal_Phone: Number,
+  contact1_Name: String,
+  contact1_Email: String,
+  contact1_Phone: Number,
+  medical_Data: String,
+  talk_ability: Boolean,
+  understand_ability: Boolean,
+  // known_Languages: String,
+  updated_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('cardID', cardIDSchema);
