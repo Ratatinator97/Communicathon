e4dc9b09ae9ab0c5c29e44684bad2ede2927a3da
Alexandros SIDIRAS <alex@Arch2.localdomain>
2019-05-04 01:49:12 +0200

    Added cors

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,13 +3,14 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const port = 4000;
 const app = express();
 const router = express.Router();
 const user_route = require('./routes/route_user');
 const cardID_route = require('./routes/route_cardID');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 /* DATABASE CONNECTION */
 
 mongoose.connect('mongodb://localhost/database');
