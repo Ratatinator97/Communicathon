d64ede279839ba569f7ea12d5b3feb97178e5186
Alexandros SIDIRAS <alex@Arch2.localdomain>
2019-05-03 22:26:03 +0200

    Connecting Components to the service

--- frontEnd/frontEnd/src/app/card-id/create/create.component.ts
+++ frontEnd/frontEnd/src/app/card-id/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CardIDService} from '../../card-id.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cardIDservice: CardIDService) { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- frontEnd/frontEnd/src/app/card-id/edit/edit.component.ts
+++ frontEnd/frontEnd/src/app/card-id/edit/edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CardIDService} from '../../card-id.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cardIDservice: CardIDService) { }
 
   ngOnInit() {
   }
