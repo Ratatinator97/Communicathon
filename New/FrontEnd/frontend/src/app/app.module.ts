9fdb9b1310481c726652cccdbaba74e2823a7893
Alexandros SIDIRAS <alex@Arch2.localdomain>
2019-05-07 17:39:21 +0200

    ok

--- New/FrontEnd/frontend/src/app/app.module.ts
+++ New/FrontEnd/frontend/src/app/app.module.ts
@@ -3,7 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatOptionModule, 
+  MatSelectModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatCardModule, 
+  MatTableModule, 
+  MatDividerModule,
+  MatSnackBarModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
