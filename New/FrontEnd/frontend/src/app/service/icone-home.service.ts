3357915743c3924645417ab8faf8dbb04dee949b
CassandreLB <cassandreleborgne0@gmail.com>
2019-05-21 17:37:10 +0200

    img

--- New/FrontEnd/frontend/src/app/service/icone-home.service.ts
+++ New/FrontEnd/frontend/src/app/service/icone-home.service.ts
@@ -5,7 +5,7 @@ export class HomeIconeService {
             nomIcone:"Carte d'identité",
             classe: "carteid",
             dirigeVers:"cardid/list",
-            lienImage: "https://maessage.files.wordpress.com/2014/06/7-emoji-bust-in-silhouette.png"
+            lienImage: "http://localhost:4000/images/ic-carteid.png"
 
         },
 
@@ -13,35 +13,35 @@ export class HomeIconeService {
             nomIcone:"Calendrier",
             classe: "calendar",
             dirigeVers:"",
-            lienImage: "http://www.courirenmoselle.fr/wp-content/uploads/2018/12/calendrier2019.png"
+            lienImage: "http://localhost:4000/images/ic-calendrier.png"
         },
 
         {
             nomIcone:"Photos",
             classe: "photo",
             dirigeVers:"image",
-            lienImage: "https://pngimage.net/wp-content/uploads/2018/06/logo-appareil-photo-png-2.png"
+            lienImage: "http://localhost:4000/images/ic-photo.png"
         },
 
         {
             nomIcone:"Videos",
             classe: "video",
             dirigeVers:"",
-            lienImage: "https://cdn.icon-icons.com/icons2/495/PNG/512/video-clip-3_icon-icons.com_48391.png"
+            lienImage: "http://localhost:4000/images/ic-video.png"
         },
 
         {
             nomIcone:"Liens",
             classe: "lien",
             dirigeVers:"links/list",
-            lienImage: "https://cdn.icon-icons.com/icons2/692/PNG/512/seo-social-web-network-internet_58_icon-icons.com_61529.png"
+            lienImage: "http://localhost:4000/images/ic-lien.png"
         },
 
         {
             nomIcone:"Carnet de Bord",
             classe: "fiche",
             dirigeVers:"ficheWE/list",
-            lienImage: "https://tse4.mm.bing.net/th?id=OIP.P_EbrcStTLkrwXp7Xw0hfQHaHa&pid=Api&P=0&w=300&h=300"
+            lienImage: "http://localhost:4000/images/ic-carnet.jpeg"
         }
           
     ];
